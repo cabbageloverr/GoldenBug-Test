bee7d6db5e71da13cc3ff4b449452e04abefafb3
Natthima Noinon <122480510+cabbageloverr@users.noreply.github.com>
2024-05-29 02:39:19 +0700

    add border when open dropdown

--- src/Modules/Register/InputDropdown.vue
+++ src/Modules/Register/InputDropdown.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <div ref="dropdown" class="relative">
-            <input @click="openDropdown" v-model="selectedTect"
+            <input type="button" @click="openDropdown" v-model="selectedTect"
                 class="input-field text-left bg-gray-50 border border-gray-300 text-sm sm:text-base rounded-full focus:border-sky-700 focus:border-sky-700 block w-full p-2"
                 :class="text ? 'text-gray-900' : 'text-gray-400'" />
 
